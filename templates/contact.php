aa9f181f970febb1dd14e83ab945e78d3caa00ee
Sean Mongey <sm@smongey.com>
2014-07-25 12:02:24 +0100

    fixed news arrows and contact slider

--- templates/contact.php
+++ templates/contact.php
@@ -4,7 +4,18 @@ include("./head.inc");
 
 <div class="contact">
 	<div class="image">
-		<img src="<?php echo $page->contact_graphic->url; ?>" alt="">
+		<div id="sustainability">
+		<?php
+
+		if (count($page->contact_graphic)) {         
+			foreach($page->contact_graphic as $image) {       
+				$img = $image->size(600, 500);      
+				echo "<img src='{$img->url}' alt='{$image->description}' />";
+			}     
+		}
+
+		?>
+		</div>
 	</div>
 	<div class="text">
 		<p><?php echo $page->title; ?></p>
--- templates/entry.php
+++ templates/entry.php
@@ -7,7 +7,7 @@ include("./head.inc");
 
 <div class="news">
 	<div class="image">
-		<?php $main = $page->news_image->first(); ?>
+		<?php $main = $page->news_image->first ?>
 		<img src="<?php echo $main->url ?>" alt="">
 	</div>
 	<div class="text">
@@ -26,44 +26,5 @@ include("./head.inc");
 	<?php if ($page->next->url !== '') { ?>
 	<a class="arrow nextlink" href="<?php echo $page->next->url ?>"></a>
 	<?php } ?>
-
-
-<!--
-		<div class="news">
-			
-			<div id="container">
-
-				<div class="full">
-
-					<?php 
-
-					$name = $page->news_author->name;
-					$u = $users->get($name);
-					$mainprofile = $u->person_photo;
-					$mainprofile = $mainprofile->size(100,100);
-
-					$main = $page->news_image->first();
-					$main = $main->size(1000,500);
-
-					?>
-
-					<div class="image">
-						<h2><?php echo "{$page->title}"; ?></h2>
-						<img src="<?php echo "{$main->url}"; ?>" alt=''>
-					</div>
-					<div class="text">
-						<div class="profile">
-							<img src="<?php echo "{$mainprofile->url}" ?>" alt="">
-							<p>Published on <span><?php echo date("j M Y", $page->created); ?></span> by <span><?php echo "{$page->news_author->name}"; ?></span></p>
-						</div>
-						<div class="intro">
-							<?php echo "{$page->news_content}"; ?>
-						</div>
-					</div>
-				</div>
-			</div> 
-					
-		</div>
--->
 
 <?php include("./foot.inc"); ?>
