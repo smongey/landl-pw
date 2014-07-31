3f275b67d0a8bf9cc6dcde2d5ea0efbbdf5b3e48
Sean Mongey <sm@smongey.com>
2014-07-31 17:31:10 +0100

    added slider to studio

--- templates/studio.php
+++ templates/studio.php
@@ -4,7 +4,18 @@ include("./head.inc");
 
 <div class="studio">
 	<div class="image">
-		<img src="<?php echo $page->studio_graphic->url; ?>" alt="">
+		<div id="sustainability">
+		<?php
+
+		if (count($page->studio_graphic)) {         
+			foreach($page->studio_graphic as $image) {       
+				$img = $image->size(600, 500);      
+				echo "<img src='{$img->url}' alt='{$image->description}' />";
+			}     
+		}
+
+		?>
+		</div>
 	</div>
 	<div class="text">
 		<?php echo $page->body; ?>
